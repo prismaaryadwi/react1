53674bcc45864bca7f3eee9bc8d8d7badd7bce30
prismaaryadwi <prisma.aryadwi@gmail.com>
2026-01-17 19:29:38 +0700

    fix2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/react1",
+  base: import.meta.env.VITE_BASE_PATH,
 });
-
